19fc57f0daf07fd876674309b4351d83e1fd22e4
arslonga83 <jlwood83@gmail.com>
2022-07-21 14:32:33 -0700

    character limit added to phone field, jscript code to verify passwords match and warn if they don't

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const pwd1 = document.querySelector("#pwd");
 const pwd2 = document.querySelector("#pwd2");
 const button = document.querySelector("#submit");
+const verify = document.querySelector("#pwd-verify");
 
-function logPwd() {
-    console.log(pwd1);
-    console.log(pwd2);
-}
+button.addEventListener('click', () => {
+    if (pwd1.value == pwd2.value) {
+        return;
+    }
+    else {
+        alert('Please make sure passwords match');
+    }
+})
 
+pwd2.addEventListener('input', () => {
+    if (pwd1.value == pwd2.value) {
+        pwd2.style.borderColor = "green"
+        verify.textContent = "";
+    }
+    else {
+        pwd2.style.borderColor = "red";
+        verify.textContent = "Passwords do not match";
+    }
+})
 
-button.addEventListener('click', (logPwd))
+pwd1.addEventListener('input', () => {
+    if (pwd1.value == pwd2.value) {
+        pwd2.style.borderColor = "green"
+        verify.textContent = "";
+    }
+    else {
+        pwd2.style.borderColor = "red";
+        verify.textContent = "Passwords do not match";
+    }
+})
 
 
 
