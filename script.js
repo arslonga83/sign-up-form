3c3c4edf34a9a570614b624b1ca7f9d3e0270741
arslonga83 <jlwood83@gmail.com>
2022-07-20 22:41:26 -0700

    create jscript queryselectors

---
+++ script.js
@@ -0,0 +1,14 @@
+const pwd1 = document.querySelector("#pwd");
+const pwd2 = document.querySelector("#pwd2");
+const button = document.querySelector("#submit");
+
+function logPwd() {
+    console.log(pwd1);
+    console.log(pwd2);
+}
+
+
+button.addEventListener('click', (logPwd))
+
+
+
